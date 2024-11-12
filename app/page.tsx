bfdb32ad0ff31bbfbe6702b1e8eca1472cae4d0a
giojano1 <giorgi.janelidze64@gmail.com>
2024-11-12 17:25:00 +0400

    bugFix/undefined=card

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ import { Card } from "@/types";
 import { cn } from "@/lib/utils";
 export default function Home() {
   const [cards, setCards] = useState<Card[]>(() => {
-    if (typeof window != "undefined") {
+    if (typeof window !== "undefined") {
       const savedCards = localStorage.getItem("cards");
       return savedCards ? JSON.parse(savedCards) : [];
     }
+    return [];
   });
   // save cards to local storage
   useEffect(() => {
@@ -82,80 +83,85 @@ export default function Home() {
       link.click();
     }
   };
+
   return (
     <div>
       <AddCardButton setCards={setCards} />
       <div className="flex flex-col items-center mx-auto my-5 gap-5 w-[320px]">
-        {cards.map((card) => (
-          <div
-            key={card.id}
-            className={cn(
-              `w-full min-h-[200px] shadow rounded-lg p-5`,
-              card.bg === "yellow" && "bg-yellow-200",
-              card.bg === "green" && "bg-green-200",
-              card.bg === "blue" && "bg-blue-200",
-              card.bg === "pink" && "bg-pink-200"
-            )}
-          >
-            <div className="flex items-center justify-end gap-3">
-              <button
-                onClick={() => handleRemoveCard(card.id)}
-                className="bg-white text-red-600 px-2 rounded-full"
-              >
-                X
-              </button>
-              <button
-                onClick={() => handleDownloadTasks(card.id)}
-                className="px-2 bg-blue-600 rounded-full"
-              >
-                SAVE
-              </button>
-            </div>
-            <div className="flex items-center mt-6">
-              <input
-                className="flex-1 bg-white rounded-md px-2 h-10 text-black"
-                placeholder="Type task"
-                type="text"
-                value={taskInputs[card.id] || ""}
-                onChange={(e) => handleInputChange(card.id, e.target.value)}
-              />
-              <button
-                onClick={() => handleAddTask(card.id)}
-                className="bg-green-700 ml-2 px-4 rounded-md h-10"
-              >
-                Add
-              </button>
-            </div>
-            <div className="my-5 flex flex-col gap-2">
-              {card.tasks.map((task) => (
-                <div
-                  key={task.id}
-                  className="flex items-start justify-between text-black  "
+        {cards.length > 0 ? (
+          cards.map((card) => (
+            <div
+              key={card.id}
+              className={cn(
+                `w-full min-h-[200px] shadow rounded-lg p-5`,
+                card.bg === "yellow" && "bg-yellow-200",
+                card.bg === "green" && "bg-green-200",
+                card.bg === "blue" && "bg-blue-200",
+                card.bg === "pink" && "bg-pink-200"
+              )}
+            >
+              <div className="flex items-center justify-end gap-3">
+                <button
+                  onClick={() => handleRemoveCard(card.id)}
+                  className="bg-white text-red-600 px-2 rounded-full"
                 >
-                  <input
-                    onChange={() => handleTaskStatusChange(task.id)}
-                    type="checkbox"
-                    checked={task.isComplete}
-                  />
-                  <span
-                    className={cn(
-                      `text-black`,
-                      task.isComplete && "line-through"
-                    )}
+                  X
+                </button>
+                <button
+                  onClick={() => handleDownloadTasks(card.id)}
+                  className="px-2 bg-blue-600 rounded-full"
+                >
+                  SAVE
+                </button>
+              </div>
+              <div className="flex items-center mt-6">
+                <input
+                  className="flex-1 bg-white rounded-md px-2 h-10 text-black"
+                  placeholder="Type task"
+                  type="text"
+                  value={taskInputs[card.id] || ""}
+                  onChange={(e) => handleInputChange(card.id, e.target.value)}
+                />
+                <button
+                  onClick={() => handleAddTask(card.id)}
+                  className="bg-green-700 ml-2 px-4 rounded-md h-10"
+                >
+                  Add
+                </button>
+              </div>
+              <div className="my-5 flex flex-col gap-2">
+                {card.tasks.map((task) => (
+                  <div
+                    key={task.id}
+                    className="flex items-start justify-between text-black  "
                   >
-                    {task.title}
-                  </span>
-                  <button
-                    onClick={() => handleTaskRemove(task.id)}
-                    className="font-bold text-red-500 text-xl"
-                  >
-                    X
-                  </button>
-                </div>
-              ))}
+                    <input
+                      onChange={() => handleTaskStatusChange(task.id)}
+                      type="checkbox"
+                      checked={task.isComplete}
+                    />
+                    <span
+                      className={cn(
+                        `text-black`,
+                        task.isComplete && "line-through"
+                      )}
+                    >
+                      {task.title}
+                    </span>
+                    <button
+                      onClick={() => handleTaskRemove(task.id)}
+                      className="font-bold text-red-500 text-xl"
+                    >
+                      X
+                    </button>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p className="text-black">No Cards</p>
+        )}
       </div>
     </div>
   );
