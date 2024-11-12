7cbdabbfecb99fa8e191837c57d43589d23d93af
giojano1 <giorgi.janelidze64@gmail.com>
2024-11-12 12:51:17 +0400

    feature/header

---
+++ components/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <header className="w-full h-14 bg-blue-950 text-xl flex items-center  justify-center">
+      2024 My Notes App
+    </header>
+  );
+};
+
+export default Footer;
---
+++ components/header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="w-full h-14 bg-blue-950 text-2xl flex items-center px-5">
+      My Notes App
+    </header>
+  );
+};
+
+export default Header;
